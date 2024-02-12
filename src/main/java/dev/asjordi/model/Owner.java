0d4e292e65d8298eab9b27ec0c5bc8258729c56e
Jordi Ayala <jord7as@gmail.com>
2024-02-12 11:40:54 -0600

    Add models

--- src/main/java/dev/asjordi/model/Owner.java
+++ src/main/java/dev/asjordi/model/Owner.java
@@ -6,4 +6,66 @@ package dev.asjordi.model;
  */
 public class Owner {
     
+    private Integer id;
+    private String name;
+    private String phone;
+    private String address;
+
+    public Owner() {}
+
+    public Owner(String name, String phone, String address) {
+        this.name = name;
+        this.phone = phone;
+        this.address = address;
+    }
+
+    public Owner(Integer id, String name, String phone, String address) {
+        this(name, phone, address);
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Owner{");
+        sb.append("id=").append(id);
+        sb.append(", name=").append(name);
+        sb.append(", phone=").append(phone);
+        sb.append(", address=").append(address);
+        sb.append('}');
+        return sb.toString();
+    }
+    
 }
--- src/main/java/dev/asjordi/model/Pet.java
+++ src/main/java/dev/asjordi/model/Pet.java
@@ -6,4 +6,110 @@ package dev.asjordi.model;
  */
 public class Pet {
     
+    private Integer id;
+    private String petName;
+    private String dogBreed;
+    private String color;
+    private Boolean allergic;
+    private Boolean specialAttention;
+    private String notes;
+    private Owner owner;
+
+    public Pet() {}
+
+    public Pet(String petName, String dogBreed, String color, Boolean allergic, Boolean specialAttention, String notes, Owner owner) {
+        this.petName = petName;
+        this.dogBreed = dogBreed;
+        this.color = color;
+        this.allergic = allergic;
+        this.specialAttention = specialAttention;
+        this.notes = notes;
+        this.owner = owner;
+    }
+
+    public Pet(Integer id, String petName, String dogBreed, String color, Boolean allergic, Boolean specialAttention, String notes, Owner owner) {
+        this(petName, dogBreed, color, allergic, specialAttention, notes, owner);
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPetName() {
+        return petName;
+    }
+
+    public void setPetName(String petName) {
+        this.petName = petName;
+    }
+
+    public String getDogBreed() {
+        return dogBreed;
+    }
+
+    public void setDogBreed(String dogBreed) {
+        this.dogBreed = dogBreed;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Boolean getAllergic() {
+        return allergic;
+    }
+
+    public void setAllergic(Boolean allergic) {
+        this.allergic = allergic;
+    }
+
+    public Boolean getSpecialAttention() {
+        return specialAttention;
+    }
+
+    public void setSpecialAttention(Boolean specialAttention) {
+        this.specialAttention = specialAttention;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Pet{");
+        sb.append("id=").append(id);
+        sb.append(", petName=").append(petName);
+        sb.append(", dogBreed=").append(dogBreed);
+        sb.append(", color=").append(color);
+        sb.append(", allergic=").append(allergic);
+        sb.append(", specialAttention=").append(specialAttention);
+        sb.append(", notes=").append(notes);
+        sb.append(", owner=").append(owner);
+        sb.append('}');
+        return sb.toString();
+    }
+    
 }
